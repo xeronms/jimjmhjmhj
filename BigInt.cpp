0cddba633e4ef7a076c4871498b083149fba5ee5
xeronms <34057264+xeronms@users.noreply.github.com>
2018-04-11 01:20:56 +0200

    Add files via upload

--- BigInt.cpp
+++ BigInt.cpp
@@ -14,56 +14,141 @@ BigInt::BigInt(int a = 0){
 
 	int b = a;
 	len = 1;
-	// how long the integer is
 	for (unsigned int j=0; b; ++j){
 		++len;
 		b /= 10;
 	}
-	num = new char [len];
+	dig = new char [len];
 
 	for (unsigned int i=2; a; ++i){
-		num[len - i] = (a % 10) + 48;
+		dig[len - i] = (a % 10) + 48;
 		a /= 10;
 	}
-	num[len -1] = '\0';
+	dig[len -1] = '\0';
+}
+
+BigInt::BigInt(bool p, unsigned int l):len(l),positive(p){
+	dig = new char [len];
 }
 
 BigInt::BigInt(char* a){
 	if (a[0] == '-'){
 		positive = 0;
 		len = strlen(a);
-		num = new char [len];
-		strcpy(num, a+1);
+		dig = new char [len];
+		strcpy(dig, a+1);
 	}
 	else{
 		positive = 1;
 		len = strlen(a) + 1;
-		num = new char [len];
-		strcpy(num, a);
+		dig = new char [len];
+		strcpy(dig, a);
 	}
 }
 
 BigInt::BigInt(const BigInt& a){
 	len=a.len; positive = a.positive;
-	num = new char [a.len];
-	memcpy(num, a.num, a.len*sizeof(char));
+	dig = new char [a.len];
+	memcpy(dig, a.dig, a.len*sizeof(char));
 }
 
 BigInt::~BigInt(){
-	delete [] num;
+	delete [] dig;
 }
 
+
+//BigInt& BigInt::operator=(const BigInt a){}
 
 unsigned char BigInt::operator[](unsigned int i) const{
-	return this->num[this->len - (2+i)];
+	return this->dig[this->len - (2+i)];
+}
+
+bool BigInt::operator==(const BigInt& a) const{
+	if (  len != a.len ) return 0;
+	for ( unsigned int i = 0; i < len-1; ++i ){
+		if ( dig[i] != a.dig[i] ) return 0;
+	}
+	return 1;
+}
+bool BigInt::operator!=(const BigInt& a) const{
+	return !(*this==a);
+}
+bool BigInt::operator>(const BigInt& a) const{
+	if (  len < a.len ) return 0;
+	if (  len > a.len ) return 1;
+	for ( unsigned int i = 0; i < len-1; ++i ){
+		if ( dig[i] < a.dig[i] ) return 0;
+		if ( dig[i] > a.dig[i] ) return 1;
+	}
+	return 0;
+}
+bool BigInt::operator<(const BigInt& a) const{
+	return a>*this;
+}
+bool BigInt::operator>=(const BigInt& a) const{
+	return !(*this<a);
+}
+bool BigInt::operator<=(const BigInt& a) const{
+	return !(*this>a);
 }
 
 
+BigInt BigInt::operator+(const BigInt& a) const{
+	if ( len < a.len ) return a+*this; // here I assume len >= a.len
+
+	if (positive&&!a.positive) return *this-(-a);
+	else if(!positive&&a.positive) return a-(-*this);
+
+	BigInt b( 1 ,len+2 );
+	char tmp = 0;
+
+	if(!positive&&!a.positive) b.positive=0;
+
+	for ( unsigned int i = 0; i < a.len-1; ++i ){
+		b.dig[i] = ((dig[i] + a.dig[i] + tmp) % 10);
+		tmp = (dig[i] + a.dig[i] + tmp) / 10;
+	}
+	if ( len == a.len ) b.dig[len-1]=tmp;
+	else {
+		for ( unsigned int i = 0; i < len-1; ++i){
+			b.dig[i] = ((dig[i] + tmp) % 10);
+			tmp = (dig[i] + tmp) / 10;
+		}
+	}
+	b.dig[ len - 1] = tmp;
+	b.dig[ len ] = '\0';
+	return b;
+}
+
+BigInt BigInt::operator-(const BigInt& a) const{
+	if ( len < a.len || ( len == a.len && *this < a )) return -(a-*this);
+
+	if (positive&&!a.positive) return *this+(-a);
+	else if(!positive&&a.positive) return a+(-*this);
+
+	BigInt b( 1 ,len+2 );
+	char tmp;
+
+	if(!positive&&!a.positive) b.positive=0;
 
 
+
+	return b;
+
+
+}
+
+BigInt BigInt::operator-() const{
+	BigInt a(*this);
+	a.positive = !positive;
+	return a;
+}
+
+
+//ew moge zrobic tak ¿e BigInt b( buf.size ) i potem a = b ale nwm czy tak mozna
 std::istream& operator>>(std::istream& is, BigInt& a){
-	delete [] a.num;
 	std::string buf;
+	unsigned int prev_a_len = a.len;
 
 	is >> buf;
 
@@ -72,18 +157,27 @@ std::istream& operator>>(std::istream& is, BigInt& a){
 		buf.erase(0);
 	}
 	else a.positive = 1;
-	a.len = buf.size();
-	strcpy(a.num,buf.data());
+	a.len = buf.size()+1;
 
+	// if len < size
+	// THROW BUFF SIZE ERROR
+	
+	for ( unsigned int i = 0; i < a.len - 1; ++i){
+		a.dig[i] = buf[i];
+		printf("%c\t",buf[i]);
+	}
 
+	for ( unsigned int i = a.len - 1; i < prev_a_len ; ++i){
+		a.dig[i] = '\0';
+	}
 	return is;
 }
 
 std::ostream& operator<<(std::ostream& os, const BigInt& a){
 	unsigned int i = 0;
 	if(!a.positive) os << '-';
-	while( a.num[i] != '\0'){
-		os << a.num[i];
+	while( a.len - (i+1) ){
+		os << a.dig[i];
 		++i;
 	}
 	 os << std::endl;
@@ -91,13 +185,13 @@ std::ostream& operator<<(std::ostream& os, const BigInt& a){
 }
 
 int main(){
-	BigInt a("-124369512347032890465290583910294852306985702589310975130982563895328903528093529803529850328903592171231281234578469035699");
-	BigInt b(-39290);
+	BigInt a("123");
+	BigInt b(290);
 	BigInt c(b);
-	BigInt d;
+	BigInt d(1);
 
-	std::cin >> d;
-	std::cout << a[16] <<std::endl;
-	std::cout << a << b << c << d <<std::endl;
+	//std::cin >> d;
+	//std::cout << a[16] << '\t' << std::endl;
+	std::cout << a << b << c << (b+c) <<std::endl;
 	return 0;
 }
--- BigInt.h
+++ BigInt.h
@@ -3,19 +3,31 @@
 #include <iostream>
 
 class BigInt{
-	char * num;
+	char * dig;
 	unsigned int len;
+	// len = numbers of digits + '\0'
 	bool positive; 
 
 public:
 	BigInt(int);
 	BigInt(char*);
+	BigInt(bool, unsigned int);
 	BigInt(const BigInt&);
 	~BigInt();
 	BigInt& operator=(const BigInt);
 
 	unsigned char operator[](unsigned int) const;
 	BigInt operator+(const BigInt&) const;
+	BigInt operator-(const BigInt&) const;
+	BigInt operator-() const;
+
+	bool operator==(const BigInt&) const;
+	bool operator!=(const BigInt&) const;
+	bool operator>(const BigInt&) const;
+	bool operator<(const BigInt&) const;
+	bool operator>=(const BigInt&) const;
+	bool operator<=(const BigInt&) const;
+
 
 	friend std::istream& operator>>(std::istream&, BigInt&);
 	friend std::ostream& operator<<(std::ostream&, const BigInt&);
